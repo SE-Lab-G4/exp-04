5a4b6b8432ef12119294abe80a6b1a3caf8d6546
halfblood <arashyadegari0402@gmail.com>
2025-05-02 20:48:15 +0330

    Add RunnerTest.java for both calculator and appointment projects

---
+++ src/test/java/appointment/RunnerTest.java
@@ -0,0 +1,13 @@
+package appointment;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        glue = "appointment"
+)
+public class RunnerTest {
+}
