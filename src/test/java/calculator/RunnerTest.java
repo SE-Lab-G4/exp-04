96c5b546f28abeace5882be0fcb460fff0677d3f
avacyrus10 <avacyrus0@gmail.com>
2025-05-02 21:16:47 +0330

    Update RunnerTest.java

--- src/test/java/calculator/RunnerTest.java
+++ src/test/java/calculator/RunnerTest.java
@@ -1,10 +1,14 @@
 package calculator;
+
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(
+    features = "src/test/resources/features",
+    glue = {"calculator", "appointment"},
+    plugin = {"pretty"}
+)
 public class RunnerTest {
 }
